5357dd09ce7321998be24849995fec6a075389e2
Jjooaogab <santosjoaogabirl09@gmail.com>
2024-06-03 23:18:46 -0300

    Fix: Change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>dev.jjooaogab</title>
+      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
